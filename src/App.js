ac4bc146b1790ee30670398070015a68c592cb17
Maria Jose <mariabecerrab17@gmail.com>
2024-04-16 10:54:15 -0500

    feat: add localStorage

--- src/App.js
+++ src/App.js
@@ -5,22 +5,68 @@ import { TodoItem } from "./components/TodoItem";
 import { CreateTodoButton } from "./components/CreateButton";
 import React from "react";
 
-const defaultTodos = [
-  {text: "Aprender maquetacion", completed: true},
-  {text: "Tomar el curso de Introduccion a React.js", completed: false},
-  {text: "Tomar agua", completed: true},
-  {text: "Llorar con la Llorona", completed: false},
-  {text: "Usar estados derivados", completed: true},
-];
+// const defaultTodos = [
+//   {text: "Aprender maquetacion", completed: true},
+//   {text: "Tomar el curso de Introduccion a React.js", completed: false},
+//   {text: "Tomar agua", completed: true},
+//   {text: "Llorar con la Llorona", completed: false},
+//   {text: "Usar estados derivados", completed: true},
+// ];
+
+// localStorage.setItem("TODOS_V1", JSON.stringify(defaultTodos));
+
+
 
 function App() {
-  const [todos, setTodos] = React.useState(defaultTodos);
+  const localStorageTodos = localStorage.getItem('TODOS_V1');
+
+  let parsedTodos;
+
+  if (!localStorageTodos) {
+    localStorage.setItem("TODOS_V1", JSON.stringify([]));
+    parsedTodos = [];
+  } else {
+    parsedTodos = JSON.parse(localStorageTodos);
+  }
+
+  const [todos, setTodos] = React.useState(parsedTodos);
   const [searchValue, setSearchValue] = React.useState('');
 
   const completedTodos = todos.filter(todo => !!todo.completed).length;
   const totalTodos = todos.length;
 
-  console.log("Los usuarios buscan de" + searchValue);
+  const serachedTodos = todos.filter(
+    (todo) => {
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+      return todoText.includes(searchText);
+    }
+  );
+
+  const saveTodos = (newTodos) => {
+    localStorage.setItem('TODOS_V1', JSON.stringify(newTodos));
+    setTodos(newTodos);
+  };
+
+  const completeTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex(
+      (todo) => todo.text === text
+    );
+    newTodos[todoIndex].completed = true;
+    saveTodos(newTodos);
+  };
+  const deleteTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex(
+      (todo) => todo.text === text
+    );
+    newTodos.splice(todoIndex, 1);
+    saveTodos(newTodos);
+  };
+
+
+
   return (
     <>
       <TodoCounter completed={completedTodos} total={totalTodos}/>
@@ -30,11 +76,14 @@ function App() {
       />
 
       <TodoList>
-      {defaultTodos.map(todo => (
+      {serachedTodos.map(todo => (
         <TodoItem
          key={todo.text}
          text={todo.text}
-         completed={todo.completed}/>
+         completed={todo.completed}
+         onComplete={(() => completeTodo(todo.text))}
+         onDelete={(() => deleteTodo(todo.text))}
+          />
         ))}
       
       </TodoList>
--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -1,16 +1,21 @@
 import "./TodoItem.css";
-import UncompleteIcon from "../../assets/UncompleteIcon.png";
-import CompleteIcon from "../../assets/CompleteIcon.png";
-import DeleteIcon from "../../assets/DeleteIcon.png";
-import DeleteIconRed from "../../assets/DeleteIconRed.png";
+import { ImCross } from "react-icons/im";
+import { FaCheck } from "react-icons/fa";
 
 function TodoItem(props) {
   return (
-    <li className="container--items">
-      <img className="completeIcon" src={props.completed ? CompleteIcon : UncompleteIcon} alt="Uncomplete" />
-
+    <li className="container--items" >
+      
+      <FaCheck 
+  className={`completeIcon ${props.completed ? 'text-green' : 'text-gray'}`} 
+  onClick= {props.onComplete}
+/>
+     
       <p>{props.text}</p>
-      <img className="deleteIcon" src={props.completed ? DeleteIconRed : DeleteIcon} alt="Delete" />
+
+      <ImCross
+       className={`deleteIcon ${props.completed ? 'text-red' : 'text-gray'}`}
+       onClick= {props.onDelete}/>
     </li>
   );
 }
